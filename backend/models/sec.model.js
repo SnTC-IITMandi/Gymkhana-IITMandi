7342053527cb4369105928e31c657617fa4a0e76
KaushikV <kaushik.vishwakarma2003@gmail.com>
2022-09-26 21:09:49 +0530

    news connected with backend

--- backend/models/sec.model.js
+++ backend/models/sec.model.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const secSchema = new Schema({
   name: { type: String, required: true },
   post: { type: String, required: true },
-  facebook: { type: string, required: false },
-  email: { type: string, required: true },
+  facebook: { type: String, required: false },
+  email: { type: String, required: true },
   img: { type: img, required: true },
   year: {type:number,required:true},
 });
